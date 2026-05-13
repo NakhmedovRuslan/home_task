71f4f43dd8cb3038afa8f9e9741847c37c268b49
Ruslan Nakhmedov <ruslan-nahmedov@mail.ru>
2026-05-13 19:17:30 +0300

    add src/utils.py, src/external_api.py, data/operations.json

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import json
+from pathlib import Path
+
+
+def operations(filename: Path) -> list[Path]:
+    """Принимает путь до JSON-файла и возвращает список словарей"""
+    try:
+        with open(filename, encoding="utf-8") as f:
+            content = json.load(f)
+
+        if isinstance(content, list):
+            return content
+        else:
+            return []
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+if __name__ == "__main__":
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    OPERATIONS_FILE = BASE_DIR / "data" / "operations.json"
+    # print(operations(OPERATIONS_FILE))
---
+++ tests/test_utils.py
@@ -0,0 +1,34 @@
+import pytest
+
+from src.utils import *
+import json
+from pathlib import Path
+
+
+def test_operations_with_file(tmp_path):
+    file = tmp_path / "testoperations.json"
+    file.write_text(json.dumps([{"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041", "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}}, "description": "Перевод организации", "from": "Maestro 1596837868705199", "to": "Счет 64686473678894779589"}]))
+
+    result = operations(file)
+    assert result ==  [{"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041", "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}}, "description": "Перевод организации", "from": "Maestro 1596837868705199", "to": "Счет 64686473678894779589"}]
+
+
+
+def test_operations_without_file():
+    result = operations("fake_operations.json")
+    assert result == []
+
+
+def test_operations_with_broken_file(tmp_path):
+    file = tmp_path / "broken_operations.json"
+    file.write_text(json.dumps("{'hello'}"))
+    result = operations(file)
+    assert result == []
+
+
+
+
+
+
+
+
